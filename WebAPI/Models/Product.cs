6cd143421fa22201be4dec0b8de510d74da28426
Ngoc Nhu <nhungoc16520@gmail.com>
2026-06-18 22:20:30 +0700

    Add project files.

---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        // Other properties
+    }
+}
